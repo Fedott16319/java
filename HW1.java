6d85185c4ddd8d348198a095047885d1d16ab47f
Дмитрий Федотов <93176555+Fedott16319@users.noreply.github.com>
2023-02-11 01:08:03 +0400

    исправленно 1 задание

--- HW1.java
+++ HW1.java
@@ -1,17 +1,25 @@
 package Homework;
+
 import java.util.Scanner;
+
 /**
- * 1. Вычислить n-ое треугольного число(сумма чисел от 1 до n), n! 
+ * 1. Вычислить n-ое треугольного число(сумма чисел от 1 до n), n! (произведение чисел от 1 до n)
  */
 public class HW1 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.printf("Введите первое число: ");
+        System.out.printf("Введите число: ");
         int i = sc.nextInt();
-        System.out.printf("Треугольное число: %d\n", giveMeNumber(i));
+        System.out.printf("Cумма треугольного числа: %d\n", summa(i));
+        System.out.printf("Произведение треугольного числа: %d\n", proizv(i));
         sc.close();
     }
-    public static int giveMeNumber(int a) {
+
+    public static int summa(int a) {
         return (a * (a + 1)) / 2;
+    }
+
+    public static int proizv(int b) {
+        return ((b * (b + 1)) / 2) * 2;
     }
 }
